f9df051f7eddbececf35772f08248bf5a0c8c6f0
pkmitl205 <58030205@kmitl.ac.th>
2017-10-27 02:23:53 +0700

    ตัวดำเนินการกำหนดค่าแบบผสม (Compound Assignment Operators)

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,7 +6,16 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            
+            int x = 6;
+            int y = 4;
+
+            Console.WriteLine(y *= 2);
+            int z = y = 3;
+
+            Console.WriteLine(z);
+            Console.WriteLine(x |= 1);
+            Console.WriteLine(x += 3);
+            Console.WriteLine(x /= 2);
         }
     }
 }
