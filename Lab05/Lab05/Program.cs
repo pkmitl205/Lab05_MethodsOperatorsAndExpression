800d48423de233e2444882ed415dcf4b27f0bac3
pkmitl205 <58030205@kmitl.ac.th>
2017-10-26 22:28:04 +0700

    การสร้างเมธอดที่รับพารามิเตอร์แบบไม่จำกัดจำนวน

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace Lab05
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        int b = add(1, 2, 3, 4, 5);
+        Console.WriteLine("b = {0}", b);
+        Console.WriteLine("1+5+8+4+1+6+6+8 = " + add(1, 5, 8, 4, 1, 6, 6, 8));
+        Console.ReadLine();
+    }
+
+    static int add(params int[] i)
+    {
+        int sum = 0; ;
+        foreach (int j in i)
+            sum += j;
+        return sum;
     }
 }
