6d539157ae7ded2e2928119f5b3a748b17e35932
pkmitl205 <58030205@kmitl.ac.th>
2017-10-27 12:53:30 +0700

    ตัวดำเนินการไบนารี่ (Binary operators) หรือตัวดำเนินการระดับบิต (Bitwise Operators)

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,7 +6,16 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            
+            byte a = 3;
+            byte b = 5;
+
+            Console.WriteLine(a | b);
+            Console.WriteLine(a & b);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(~a & b);
+            Console.WriteLine(a << 1);
+            Console.WriteLine(a << 2);
+            Console.WriteLine(a >> 1);
         }
     }
 }
