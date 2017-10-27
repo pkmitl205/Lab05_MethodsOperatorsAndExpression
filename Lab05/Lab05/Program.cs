a50e7741c40efe130e945ffbf632c0e5c45c5fef
pkmitl205 <58030205@kmitl.ac.th>
2017-10-27 14:35:27 +0700

    การบังคับให้เกิด Exception ในกรณีที่มีความผิดปกติในการแปลงชนิดข้อมูล (try..catch เพื่อจัดการกับ exception)

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,10 +6,17 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            double d = 5e9d;
-            Console.WriteLine(d);
-            int i = checked((int)d);
-            Console.WriteLine(i);
+            try
+            {
+                double d = 5e9d;
+                Console.WriteLine(d);
+                int i = checked((int)d);
+                Console.WriteLine(i);
+            }
+           catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
