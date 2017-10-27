af86b964976d6808325110f514f4240e8b5fe4cd
pkmitl205 <58030205@kmitl.ac.th>
2017-10-27 12:51:35 +0700

    ตัวดำเนินการตรรกะ (Logical operators)

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,7 +6,17 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a && b);
+            Console.WriteLine(a || b);
+            Console.WriteLine(!b);
+            Console.WriteLine(b || true);
+            Console.WriteLine((9 > 7) ^ (a == b));
+            Console.WriteLine((b != a) || (a == b));
+            Console.WriteLine((b != a) && (a == b));
+            Console.WriteLine((!a) && (!b));
+            Console.WriteLine((!a) || (!b));
         }
     }
 }
