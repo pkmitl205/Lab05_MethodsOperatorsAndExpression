f6314aea6056246c74cf0e7ae0701043dcb9859f
pkmitl205 <58030205@kmitl.ac.th>
2017-10-27 21:05:09 +0700

    แบบฝึกหัดข้อที่ 2

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,7 +6,38 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Console.Write("Please enter First Number : ");
+                int val1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Please enter Final Number : ");
+                int val2 = Convert.ToInt32(Console.ReadLine());
+
+                Mod7(val1, val2);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        static void Mod7(int val1, int val2)
+        {
+
+            for (int i = val1; i <= val2; i++) 
+            {
+                if (i % 7 == 0) 
+                {
+                    Console.Write(i+" ");
+                }
+            }
+
         }
     }
 }
