48c76237a4e75998ea4e487b9239f21a1338e79b
pkmitl205 <58030205@kmitl.ac.th>
2017-10-27 13:07:28 +0700

    ตัวดำเนินการเชื่อมข้อความ (string concatenation)

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,7 +6,12 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            
+            string hello = "Hello";
+            string world = "World";
+            string helloWorld = hello + world;
+            Console.WriteLine(helloWorld);
+            string helloWorld555 = helloWorld + " " + 555;
+            Console.WriteLine(helloWorld555);
         }
     }
 }
